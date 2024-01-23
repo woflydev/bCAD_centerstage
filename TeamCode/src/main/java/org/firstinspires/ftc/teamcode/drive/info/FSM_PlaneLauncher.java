2df3e5ee418c37eada6c2ba5a70b9f6aae84afde
woflydev <wolfy.coding@gmail.com>
2024-01-24 08:35:51 +1000

    CHORE: revert old

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/info/FSM_PlaneLauncher.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.drive.info;
-
-public enum FSM_PlaneLauncher {
-    IDLE,
-    ACTIVE,
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/info/RobotState.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.drive.info;
-
-public enum RobotState {
-    IDLE,
-    IN_MOTION,
-    TRANSFER_STAGE_ACTIVE,
-    TRANSFER_STAGE_STANDBY,
-    RUNNING_MACRO,
-}
